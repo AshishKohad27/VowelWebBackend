1804cd442723417c7a5d35cbfb0c59d27dbd6db3
AshishKohad27 <kohadashish27@gmail.com>
2023-02-15 17:08:12 +0530

    data routes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connect = require("./Config/db");
-
+const dataRoute = require("./Routes/data.route");
 
 const PORT = process.env.PORT;
 
@@ -10,6 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use("/data", dataRoute);
 
 
 app.get("/", async (req, res) => {
