5b075a26606110a062e20cd35320a6fd6504e13e
AshishKohad27 <kohadashish27@gmail.com>
2023-02-15 17:09:56 +0530

    user route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const connect = require("./Config/db");
 const dataRoute = require("./Routes/data.route");
+const userRoutes = require("./Routes/user.route");
+
 
 const PORT = process.env.PORT;
 
@@ -11,6 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/data", dataRoute);
+app.use("/user", userRoutes);
 
 
 app.get("/", async (req, res) => {
